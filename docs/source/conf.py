8dd1e86a4de000947e814349c7f3bc308c716428
little-cheng <62006789+little-cheng@users.noreply.github.com>
2023-11-24 20:32:33 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-import sphinx_rtd_theme
+
 extensions = []
 
 templates_path = ['_templates']
